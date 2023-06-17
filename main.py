f71957ada5297281c72ebac0079b875338975153
Robert Whitaker (robwhita) <robwhita@cisco.com>
2023-06-17 14:40:47 -0400

    Add function to static_paths.py

--- main.py
+++ main.py
@@ -12,10 +12,6 @@ import json
 import get_token
 import static_paths
 
-# User enters in the static path binding
-
-static_path = input('Please enter in the static path you would like to delete: ')
-
 # Retrieves authentication token
 
 token = get_token.get_token()
@@ -24,19 +20,31 @@ token = get_token.get_token()
 
 all_static_path_objects = static_paths.get_all_static_path_objects(token)
 
-# Converts all static path objects and their attributes to a dictionary
-# and retrieves data within 'imdata' key.
-
 all_static_path_objects_dict  = json.loads(all_static_path_objects.text)
 imdata = all_static_path_objects_dict['imdata']
 
-# Prints string to user.
+# Converts all static path objects and their attributes to a dictionary
+# and retrieves data within 'imdata' key.
 
-print((f'Your static_path {static_path} is bound at the following EPGs:\n'))
+list_of_static_ports = static_paths.get_all_static_path_t_dn_s(imdata)
+
+print((f'Below is a list of your static paths:\n'))
+
+for t_dn in list_of_static_ports:
+    print(t_dn)
+
+# User enters in the static path binding
+
+static_path = input('\nPlease enter a static path from above to find out which EPGs they are bound to: ')
+
 
 # Finds which DNs of EPGs the inputted static path is bound to.
 
 dn_list = static_paths.find_static_path_binding_to_epgs(imdata, static_path)
+
+# Prints string to user.
+
+print((f'\nYour static_path {static_path} is bound at the following EPGs:\n'))
 
 # For loop to print DNs of EPGs the inputted static path is bound to.
 
