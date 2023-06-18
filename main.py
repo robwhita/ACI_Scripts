70829544ef325fa7f25d1e4deba33c662be9e84b
Robert Whitaker (robwhita) <robwhita@cisco.com>
2023-06-18 11:40:47 -0400

    Add green text from colorama

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 
 ''' THe intent of the script is to get a list of all of your static path bindings.
- Once you recieve a list of static path bindings
- you can then enter in a specfic static path and see which EPGs it is bound to.
- This file is intended to be run as a script and importes the following modules:
+ Once you receive a list of static path bindings
+ you can then enter in a specific static path and see which EPGs it is bound to.
+ This file is intended to be run as a script and import the following modules:
 
     * get_token - Enables you to authenticate into the APIC
-      and recieve a token for subsequent API calls.
+      and receive a token for subsequent API calls.
     * static_paths -   Includes several functions to work with static ports.'''
 
 import json
+from colorama import init, Fore
 import get_token
 import static_paths
+
+
+init(autoreset=True)
 
 # Retrieves authentication token
 
@@ -28,14 +32,14 @@ imdata = all_static_path_objects_dict['imdata']
 
 list_of_static_ports = static_paths.get_all_static_path_t_dn_s(imdata)
 
-print(('Below is a list of your static paths:\n'))
+print((Fore.WHITE + '\nBelow is a list of your static paths:\n'))
 
 for t_dn in list_of_static_ports:
-    print(t_dn)
+    print(Fore.GREEN + t_dn)
 
 # User enters in the static path binding
 
-static_path = input('\nPlease enter a static path from above to find out which EPGs it is bound to: ')
+static_path = input(Fore.WHITE + '\nPlease enter a static path from above to find out which EPGs it is bound to: ')
 
 
 # Finds which DNs of EPGs the inputted static path is bound to.
@@ -44,9 +48,11 @@ dn_list = static_paths.find_static_path_binding_to_epgs(imdata, static_path)
 
 # Prints string to user.
 
-print((f'\nYour static_path {static_path} is bound at the following EPGs:\n'))
+print(Fore.WHITE + (f'\nYour static_path {static_path} is bound at the following EPGs:\n'))
 
 # For loop to print DNs of EPGs the inputted static path is bound to.
 
 for dn in dn_list:
-    print(dn)
+    print(Fore.GREEN + (dn))
+
+print('')
